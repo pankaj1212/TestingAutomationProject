9861ce8b0be172016d28f49ecacc0b381df3cee2
panka <panka@DESKTOP-5CHITFQ>
2020-11-26 01:05:54 +0530

    added global file

--- TestingAutomationProject/src/Global.java
+++ TestingAutomationProject/src/Global.java
@@ -1,4 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Global {
 
+	public static void main(String [] args)
+	{
+		System.setProperty("webdriver.chrome.driver", "/Users/panka/Downloads/chromedriver_win32/chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.officedepot.com/");
+		System.out.println(driver.getCurrentUrl());
+		
+		driver.close();
+	}
 }
